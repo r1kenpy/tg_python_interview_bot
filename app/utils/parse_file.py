a247b066da5f9ce68460e95c8a6d82590721b787
Vova Molchanov <r1ken@mail.ru>
2025-06-07 09:46:09 +0300

    Fix parse_file when one obj in file

--- app/utils/parse_file.py
+++ app/utils/parse_file.py
@@ -1,11 +1,17 @@
 import json
 from typing import Any
+from pathlib import WindowsPath
 
 
-def parse_file(path: str, encoding: str = "utf-8") -> list[list[Any]]:
+def parse_file(
+    path: str | WindowsPath, encoding: str = "utf-8"
+) -> list[list[Any]]:
     objs = []
     with open(path, "r", encoding=encoding) as f:
         data = json.load(f)
-        for obj in data:
-            objs.append([*obj.values()])
+        if isinstance(data, list):
+            for obj in data:
+                objs.append([*obj.values()])
+        else:
+            objs.append([*data.values()])
     return objs
